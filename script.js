1c8ca4789a5bfc2a79ae0c6d1790b80370568447
Angelo Lloti <skyuphigh01@gmail.com>
2024-03-04 14:13:08 -0500

    add image and nav menu

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 const API = 'https://lt.dim.codes/';
 
 document.addEventListener('DOMContentLoaded', async () => {
-	const res = await fetch(API);
-	const body = await res.json();
-	const tab = document.querySelector('.event');
-	const eventLink = document.getElementById('event');
+	const nav = document.getElementById('navigation');
+	const burger = document.querySelector('.hamburger');
 
-	const event = body.find(c => c.title.match(/pop|event/i));
-	if (event) {
-		tab.classList.add('has-event');
-		eventLink.innerText = event.title;
-		eventLink.href = event.url;
+	if (nav) {
+		for (const a of nav.querySelectorAll('a')) {
+			a.addEventListener('click', () => {
+				nav.classList.add('closed');
+				burger.classList.add('closed');
+			});
+		}
+
+		burger.addEventListener('click', () => {
+			nav.classList.toggle('closed');
+			burger.classList.toggle('closed');
+		});
 	}
 
-	tab.addEventListener('click', () => {
-		tab.classList.toggle('closed');
-	});
+	const res = await fetch(API);
+	const body = await res.json();
+
+	for (const event of body) {
+		if (!event.title.match(/pop|event/i)) continue;
+		document.getElementById('no-events')?.remove();
+		document.getElementById('events').innerHTML += `
+		<a href="${event.url}">• ${event.title} <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" viewBox="0 0 24 24"><path d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"/></svg></a>
+		`;
+	}
 });
