57693e5287cdac108abfb24a61fc3f08b1554f85
Angelo Lloti <skyuphigh01@gmail.com>
2024-03-21 18:46:14 -0400

    faq and policy

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ document.addEventListener('DOMContentLoaded', async () => {
 	const nav = document.getElementById('navigation');
 	const burger = document.querySelector('.hamburger');
 
+	for (const d of document.querySelectorAll('.chevron')) {
+		d.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" viewBox="0 0 24 24"><path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z"/></svg>';
+		d.parentElement.addEventListener('click', () => {
+			d.classList.toggle('flipped');
+			d.parentElement.parentElement.querySelector('.answer').classList.toggle('active');
+		});
+	}
+
 	if (nav) {
 		for (const a of nav.querySelectorAll('a')) {
 			a.addEventListener('click', () => {
